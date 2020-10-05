974b682ca4962d5393f559eb631bdcf0563c5896
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-05 12:37:09 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program performs the hailstone pattern
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // ask and initialize n
+    System.out.println("Please enter a positive integer to see the hailstone sequence.");
+    int theNumber = input.nextInt();
+
+    // declare the even and odd deciding variable, if evenOdd = 0 then the number is even and if the evenOdd = 1 then the number is odd
+    int evenOdd;
+
+    // create a loop that ends when n = 1
+    do {
+      // tell them the number were on
+      System.out.println(theNumber);
+
+      // give the evenOdd variable value based on the new number
+      evenOdd = theNumber % 2;
+      
+      // see if the number is even or odd, then do what the patterns tells us to do.
+      if (evenOdd == 0) {
+        theNumber = theNumber / 2;
+      } else {
+        theNumber = theNumber * 3 + 1;
+      }
+    } while (theNumber != 1);
     
   }
 }
